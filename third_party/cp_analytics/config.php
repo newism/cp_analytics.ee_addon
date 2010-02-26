4de644c716dc70d32978fa8dd063d53e146c0569
Leevi Graham <leevi@newism.com.au>
2010-02-26 13:45:00 +1100

    Added NSM Addon Updater Integration

---
+++ third_party/cp_analytics/config.php
@@ -0,0 +1,6 @@
+<?php
+
+$config['name']				= 'CP Analytics';
+$config['version']	 		= '1.1.0';
+
+$config['nsm_addon_updater']['versions_xml'] 			= 'http://github.com/amphibian/cp_analytics.ee_addon/raw/master/versions.xml';
